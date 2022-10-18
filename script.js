3c94400ee778066d05635e7af035dfc0308dd380
maxlair1 <max@lairfamily.com>
2022-10-18 18:28:00 -0400

    title js!

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const title = document.getElementById("intro-name");
 
-
-window.getEventListener('scroll', function(){
+window.addEventListener('scroll', function(){
     var value = window.scrollY
+
+    console.log(value);
+    
+    title.style.top = value * .6 + 'px';
 })
+
+
 
 
   
