bab14b6d37bce7604af0de8935096bf02a9ab7af
maxlair1 <max@lairfamily.com>
2022-10-18 20:54:31 -0400

    Scroll Rect

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const title = document.getElementById("intro-name");
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY
-
-    console.log(value);
     
     title.style.top = value * .6 + 'px';
 })
 
+function visible(){
+    var element = document.getElementById("sec-1-grid");
 
+    
+}
+
+var element = document.getElementById("sec-1-grid");
+
+console.log(element.children);
 
 
   
