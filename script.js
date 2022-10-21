c4df0a18fb7c97c35e87913a14641e6220777ad4
maxlair1 <max@lairfamily.com>
2022-10-21 00:28:09 -0400

    styling portfolio page

--- script.js
+++ script.js
@@ -6,34 +6,29 @@ window.addEventListener('scroll', function(){
     title.style.top = value * .6 + 'px';
 
     isElementInViewPort
-})
+});
 
 
-// function isElementInViewPort(element){
-//         let rect = element.getBoundingClientRect();
-//         console.log("the Bounding Rect of element is ", rect)
-//         //gets the height of the whole window.
-//         let viewPortBottom = window.innerHeight || document.documentElement.clientHeight;
+function isElementInViewPort(element){
+        let rect = element.getBoundingClientRect();
+        console.log("the Bounding Rect of element is ", rect)
+        //gets the height of the whole window.
+        let viewPortBottom = window.innerHeight || document.documentElement.clientHeight;
 
-//         let viewPortRight = window.innerWidth || document.documentElement.clientWidth;
+        let viewPortRight = window.innerWidth || document.documentElement.clientWidth;
         
-//         let isTopInViewPort = rect.top >= 0,
-//             isLeftInViewPort = rect.left >= 0,
-//             isBottomInViewPort = rect.bottom <= viewPortBottom,
-//             isRightInViewPort = rect.right <= viewPortRight;
+        let isTopInViewPort = rect.top >= 0,
+            isLeftInViewPort = rect.left >= 0,
+            isBottomInViewPort = rect.bottom <= viewPortBottom,
+            isRightInViewPort = rect.right <= viewPortRight;
         
-//     return (isTopInViewPort && isLeftInViewPort && isBottomInViewPort && isRightInViewPort);
-// }
-// let topEle = document.getElementById('intro-name');
-//     console.log("is top element visible: ", isElementInViewPort(topEle));
+    return (isTopInViewPort && isLeftInViewPort && isBottomInViewPort && isRightInViewPort);
+}
+let topEle = document.getElementById('intro-name');
+    console.log("is top element visible: ", isElementInViewPort(topEle));
 
-// let bottomEle = document.getElementById('links');
-//     console.log("is bottom element visible ", isElementInViewPort(bottomEle));
+let bottomEle = document.getElementById('links');
+    console.log("is bottom element visible ", isElementInViewPort(bottomEle));
 
-// console.log(isElementInViewPort(topEle));
-
-// while (isElementInViewPort){
-//     console.log("the element is visible!");
-// };
-
+console.log(isElementInViewPort(topEle));
   
