b8bb68130249876909bc0a68fa9a8944f91bf095
maxlair1 <max@lairfamily.com>
2022-10-28 07:53:12 -0400

    sumthin

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Home text slow scroll
 
-const title = document.getElementById("intro-name");
+const title = document.getElementById("hero-name");
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY
@@ -9,8 +9,32 @@ window.addEventListener('scroll', function(){
     title.style.top = value * .6 + 'px';
 });
 
-var introPar = document.getElementById("intro-par");
+// grab details about an element 
+
+function isElementInViewPort(element){
+    let rect = element.getBoundingClientRect();
+    console.log("the Bounding Rect of element is ", rect)
+    //gets the height of the whole window.
+    let viewPortBottom = window.innerHeight || document.documentElement.clientHeight;
+
+    let viewPortRight = window.innerWidth || document.documentElement.clientWidth;
+    
+    let isTopInViewPort = rect.top >= 0,
+        isLeftInViewPort = rect.left >= 0,
+        isBottomInViewPort = rect.bottom <= viewPortBottom,
+        isRightInViewPort = rect.right <= viewPortRight;
+
+return (isTopInViewPort && isLeftInViewPort && isBottomInViewPort && isRightInViewPort);
+}
+
+var heroLinks = document.getElementById("hero-links");
 
 function onLoadfadeUp(element){
-    
+    isElementInViewPort(element)
+    if(isTopInViewPort = true){
+        element.classList.add('MyClass');
+    } else {
+
+    }
+
 }
